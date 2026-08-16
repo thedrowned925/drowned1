f12bfc87ae298aa5fcb646806cf4e9981d943d42
thedrowned925 <chatgpticib1@gmail.com>
2026-08-16 16:15:14 +0300

    track raw manifest paths on Android

--- android/app/src/main/java/com/thedrowned/drowned/Models.kt
+++ android/app/src/main/java/com/thedrowned/drowned/Models.kt
@@ -1,6 +1,12 @@
 package com.thedrowned.drowned
 
-data class ReleaseChannel(val version: String, val tag: String, val manifestUrl: String, val size: Long)
+data class ReleaseChannel(
+    val version: String,
+    val tag: String,
+    val manifestPath: String,
+    val manifestUrl: String,
+    val size: Long
+)
 data class GameEntry(
     val id: String,
     val title: String,
