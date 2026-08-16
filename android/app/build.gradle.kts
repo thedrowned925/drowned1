547ca4f54e3ab6eebdf8ecf4bbd0c274f93d22bd
thedrowned925 <chatgpticib1@gmail.com>
2026-08-16 16:01:39 +0300

    use stable Android API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.thedrowned.drowned"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.thedrowned.drowned"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
