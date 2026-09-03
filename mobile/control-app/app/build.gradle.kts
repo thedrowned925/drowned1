d0abdce241bbd01cdbd9d59764a6a22e203647ae
thedrowned925 <chatgpticib1@gmail.com>
2026-09-03 13:53:13 +0300

    Bump Drowned Control to v1.4.0 for remote file manager

--- mobile/control-app/app/build.gradle.kts
+++ mobile/control-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.drowned.control"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
     }
 
     buildFeatures {
