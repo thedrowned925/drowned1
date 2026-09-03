2a2ed8992bbc1b8901bd1e234f3883a13dea7b6d
thedrowned925 <chatgpticib1@gmail.com>
2026-09-03 14:41:05 +0300

    Bump Drowned Control to v1.4.1

--- mobile/control-app/app/build.gradle.kts
+++ mobile/control-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.drowned.control"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.4.1"
     }
 
     buildFeatures {
